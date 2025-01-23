afd6fe901e0f00ae6f2e713accac9673c35b5541
Jonathan Herring <124759864+jonathan-herring@users.noreply.github.com>
2025-01-22 21:29:41 -0800

    tailwind setup

--- LootFinder/vite.config.ts
+++ LootFinder/vite.config.ts
@@ -1,13 +1,14 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
+import vue from '@vitejs/plugin-vue';
 import { resolve } from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [tailwindcss(), vue()],
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
     },
   },
-})
+});
