28c9ec9d140d5a2b932f0954e851f8025a20c944
Jonathan Herring <124759864+jonathan-herring@users.noreply.github.com>
2025-01-22 22:41:21 -0800

    apply tailwind and test

--- LootFinder/src/views/Home.vue
+++ LootFinder/src/views/Home.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-    <h1>Welcome to Home</h1>
+    <h1 class="text-3xl text-textPrimary font-bold underline">
+      Welcome to Home
+    </h1>
     <router-link to="/login">Go to Login</router-link>
     <br />
     <router-link to="/dashboard">Go to Dashboard</router-link>
