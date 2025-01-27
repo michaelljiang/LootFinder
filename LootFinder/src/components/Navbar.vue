577098ac4d897aabf0a4b7d1085936b13ed33876
Jonathan Herring <124759864+jonathan-herring@users.noreply.github.com>
2025-01-27 15:14:23 -0800

    Add browser offers link to navbar

--- LootFinder/src/components/Navbar.vue
+++ LootFinder/src/components/Navbar.vue
@@ -70,6 +70,12 @@
         >
           <div class="flex flex-col -mx-6 lg:flex-row lg:items-center lg:mx-8">
             <router-link
+              to="/browse-offers"
+              class="px-3 py-2 mx-3 mt-2 text-gray-700 transition-colors duration-300 transform rounded-md lg:mt-0 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700"
+            >
+              Browse Offers
+            </router-link>
+            <router-link
               to="/dashboard"
               class="px-3 py-2 mx-3 mt-2 text-gray-700 transition-colors duration-300 transform rounded-md lg:mt-0 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700"
             >
