c18ae4150ca81cb31e3f379fcaf17d56a2ffa1c0
Jonathan Herring <124759864+jonathan-herring@users.noreply.github.com>
2025-01-27 02:28:28 -0800

    add link to Create Offer button in navbar

--- LootFinder/src/components/Navbar.vue
+++ LootFinder/src/components/Navbar.vue
@@ -87,12 +87,12 @@
             >
               Random Item
             </router-link> -->
-            <a
-              href="#"
+            <router-link
+              to="/create-offer"
               class="px-3 py-2 mx-3 mt-2 text-gray-700 transition-colors duration-300 transform rounded-md lg:mt-0 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700"
             >
               Create Offer
-            </a>
+            </router-link>
           </div>
 
           <div class="flex items-center mt-4 lg:mt-0">
