d6b3d3b22956112747d3ec44a2159c9e9b5eef37
Michael Jiang <michaeljiang@ucsb.edu>
2025-01-28 23:57:14 -0800

    backend for create offer

--- LootFinder/src/firebase.ts
+++ LootFinder/src/firebase.ts
@@ -2,14 +2,19 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 import config from './config';
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
 
 // Initialize Firebase
 const firebaseApp = initializeApp(config);
 
 // Initialize Firebase Authentication
 const auth = getAuth(firebaseApp);
-
+// Initialize Firestore
+const db = getFirestore(firebaseApp);
 // Google Auth Provider
 const googleProvider = new GoogleAuthProvider();
 
-export { auth, googleProvider };
+const storage = getStorage(firebaseApp)
+
+export { auth, googleProvider,db, storage };
