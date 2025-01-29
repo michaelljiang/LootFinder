51947d867e29f2b7a8bb49f39c0da55120958b39
AjayLiu <ajayliu2928@gmail.com>
2025-01-29 15:05:33 -0800

    Add Footer and also changed app title and favicon

---
+++ LootFinder/src/components/Footer.vue
@@ -0,0 +1,21 @@
+<template>
+  <footer
+    class="bg-gray-800 text-white text-center p-4 flex justify-center items-center"
+  >
+    <p class="mr-4">2024 Winter CS 148 Team PJ14</p>
+    <div>•</div>
+    <a
+      class="flex hover:underline ml-4"
+      href="https://github.com/ucsb-cs148-w25/pj14-lootfinder"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <img
+        src="https://img.icons8.com/ios11/512/FFFFFF/github.png"
+        alt="Github Logo"
+        class="inline w-6 h-6 mr-2"
+      />
+      View Github Repo
+    </a>
+  </footer>
+</template>
