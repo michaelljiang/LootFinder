ee52dcfb6eb58102309738493a1101ab16e302a8
AwinZhang <infinitycomplix@gmail.com>
2025-02-14 14:22:38 -0800

    a

--- LootFinder/src/router/index.ts
+++ LootFinder/src/router/index.ts
@@ -9,6 +9,7 @@ import BrowseOffers from '@/views/BrowseOffers.vue';
 import UpdateOffers from '@/views/UpdateOffers.vue';
 import ChatView from '@/views/ChatView.vue';
 import Inbox from '@/views/Inbox.vue';
+import SearchBar from '@/views/SearchBar.vue';
 
 const routes = [
   {
@@ -20,6 +21,11 @@ const routes = [
     path: '/login',
     name: 'Login',
     component: Login,
+  },
+  {
+    path: '/search-bar',
+    name: 'Search Bar',
+    component: SearchBar,
   },
   {
     path: '/dashboard',
