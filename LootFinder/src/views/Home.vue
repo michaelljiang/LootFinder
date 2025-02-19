1a05a38d1c149be96d9e02c4b552f7b5234c57af
AjayLiu <ajayliu2928@gmail.com>
2025-02-19 14:22:33 -0800

    Change the lorem ipsum placeholder, rerouted some broken links

--- LootFinder/src/views/Home.vue
+++ LootFinder/src/views/Home.vue
@@ -42,8 +42,8 @@
         <div
           class="relative rounded-full px-3 py-1 text-sm/6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20"
         >
-          Post a bounty.
-          <a href="#" class="font-semibold text-indigo-600">
+          Find loot near you.
+          <a href="/browse-offers" class="font-semibold text-indigo-600">
             <span class="absolute inset-0" aria-hidden="true"></span>
             Try it out!
             <span aria-hidden="true">&rarr;</span>
@@ -55,14 +55,12 @@
         <h1
           class="text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-7xl"
         >
-          <!-- Data to enrich your online business -->
-          Opportunities to discover and trade.
+          LootFinder
         </h1>
         <p
           class="mt-8 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8"
         >
-          Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem
-          cupidatat commodo. Elit sunt amet fugiat veniam occaecat.
+          Find and sell cheap items in your area.
         </p>
 
         <div class="mt-10 flex items-center justify-center gap-x-6">
