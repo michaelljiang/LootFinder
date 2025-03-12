a52be6bd12948e198a85926644cbe4506d0c2d65
AwinZhang <infinitycomplix@gmail.com>
2025-03-12 00:20:02 -0700

    a

--- LootFinder/src/components/Navbar.vue
+++ LootFinder/src/components/Navbar.vue
@@ -60,18 +60,18 @@
           class="absolute inset-x-0 z-20 w-full px-6 py-4 transition-all duration-300 ease-in-out lg:mt-0 lg:p-0 lg:top-0 lg:relative lg:bg-transparent lg:w-auto lg:opacity-100 lg:translate-x-0 lg:flex lg:items-center"
         >
           <div class="flex flex-col -mx-6 lg:flex-row lg:items-center lg:mx-8">
+            <!-- <router-link
+              to="/browse-offers"
+              class="px-3 py-2 mx-3 mt-2 text-black transition-colors duration-300 transform rounded-md lg:mt-0 hover:bg-nav hover:text-white"
+            >
+              Browse Offers
+            </router-link> -->
             <router-link
               to="/SearchBar"
               class="px-3 py-2 mx-3 mt-2 text-black transition-colors duration-300 transform rounded-md lg:mt-0 hover:bg-nav hover:text-white"
             >
               User Search
             </router-link>
-            <router-link
-              to="/browse-offers"
-              class="px-3 py-2 mx-3 mt-2 text-black transition-colors duration-300 transform rounded-md lg:mt-0 hover:bg-nav hover:text-white"
-            >
-              Browse Offers
-            </router-link> -->
             <router-link
               to="/browse"
               class="px-3 py-2 mx-3 mt-2 text-black transition-colors duration-300 transform rounded-md lg:mt-0 hover:bg-nav hover:text-white"
